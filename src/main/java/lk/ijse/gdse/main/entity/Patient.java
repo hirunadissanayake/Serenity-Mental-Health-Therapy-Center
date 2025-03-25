5bc6d31753f92d6d9e17af8ac35e9b4660864732
hirunadissanyake <hirunasankalpa@gmail.com>
2025-03-26 02:19:17 +0530

    coplete uis

--- src/main/java/lk/ijse/gdse/main/entity/Patient.java
+++ src/main/java/lk/ijse/gdse/main/entity/Patient.java
@@ -1,4 +1,10 @@
 package lk.ijse.gdse.main.entity;
 
 public class Patient {
+    private String id;
+    private String name;
+    private String dob;
+    private String gender;
+    private String contact;
+    private String medicalHistory;
 }
